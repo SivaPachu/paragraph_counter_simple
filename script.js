4c37cb6d6191bde8c645ad3a8c29182dc8f625f0
Siva Pachu <134077029+SivaPachu@users.noreply.github.com>
2026-08-08 19:09:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var para = document.getElementById("para");
+var counter = document.getElementById("counter");
+var remaining = document.getElementById("remaining");
+var btn = document.getElementById("btn");
+
+para.addEventListener("input", function () {
+
+    var writtenCharacters = para.value.length;
+    var leftCharacters = 200 - writtenCharacters;
+
+    counter.textContent = "Written Characters: " + writtenCharacters;
+    remaining.textContent = "Left Characters: " + leftCharacters;
+});
+
+btn.addEventListener("click", function () {
+
+    para.value = "";
+
+    counter.textContent = "Written Characters: 0";
+    remaining.textContent = "Left Characters: 200";
+});
